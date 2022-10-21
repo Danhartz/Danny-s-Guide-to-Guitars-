ac3edddee5d6f4fd83b51fc477cb63e060786497
Danhartz <116391356+Danhartz@users.noreply.github.com>
2022-10-21 17:23:06 -0500

    First Paragraph
    First paragraph, opener

--- Danny'sGuidetoGuitars.py
+++ Danny'sGuidetoGuitars.py
@@ -6,3 +6,4 @@ This is a deep dive into my interests in regard to guitars and the various nuanc
 <meta charset="UTF-8">
 <title>Dannys Guide to Guitars</title>
 <script>
+print(Guitars are a quintessential element to music and its creation. With that being said there are many ways different brands and types of guitars to look at especially for beginners. First thing first is figuring out your sound, some like a clean and ethereal sound while others hunt for that distorted dirty sound.)
