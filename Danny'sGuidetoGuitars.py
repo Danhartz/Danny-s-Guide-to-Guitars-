ab28d12ec9d2e7640e911e61d377d2a5a94092c4
Danhartz <116391356+Danhartz@users.noreply.github.com>
2022-10-21 17:45:21 -0500

    Added Paragraphs and Lists

--- Danny'sGuidetoGuitars.py
+++ Danny'sGuidetoGuitars.py
@@ -6,4 +6,22 @@ This is a deep dive into my interests in regard to guitars and the various nuanc
 <meta charset="UTF-8">
 <title>Dannys Guide to Guitars</title>
 <script>
+
 print(Guitars are a quintessential element to music and its creation. With that being said there are many ways different brands and types of guitars to look at especially for beginners. First thing first is figuring out your sound, some like a clean and ethereal sound while others hunt for that distorted dirty sound.)
+
+<p>A Few of the Different Guitar Brands</p>
+
+<ul>
+<li>Fender</li>
+<li>Gibson</li>
+<li>Ibanez</li>
+<li>Paul Reed Smith</li>
+<li>Jackson</li>
+<li>Martin</li>
+<li>Taylor</li>
+
+print(The various brands mentioned above are just a few of the many guitar brands out there. All of these brands bring different sounds and tones to the table.)
+
+print(If you are going for that clean sound you are probably going to want to stick to fender, they are known for that crispy clean tone that finds itself in all genres of music from indie rock to rnb. If youre are looking for a more distorted crunchy sound Gibson guitars tend to offer exactly that utilizing the humbucker pickups which are known for a warmer heavier sound. Fender guitars tend to be made with single coil pickups that usually offer a cleaner sound. Ibanez and Jackson guitars are known for a more metal like sound that utilizes heavy distortion. Ibanez guitars in recent years have began to expand their collection adding cleaner sounding guitars that differ greatly from their tradtional metal sounding ones of the past. Paul Reed Smith guitars are kind of all over the places and even have their own copycat like designs of the Gibson Les Paul and the Fender Stratocaster. Martin and Taylor are to my knowledge strictly acoustic usually being of very high quality and expensive.)
+
+print(while this was not the most in depth dive into every guitar brand this is a good starting point for any hopeful beginner looking ot get into playing. The various brands discussed offer a wide range of differnt sounds sonically and the descriptons given are helpful in helping those wanting to get into playing to narrow down on what their first guitar might look like.)
