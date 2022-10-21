bb0b7e92a23bf238d134e51cc3a7e907fd48c716
Danhartz <116391356+Danhartz@users.noreply.github.com>
2022-10-21 17:10:04 -0500

    Quick Update
    Added What was written in the example code for the assignment

---
+++ Danny'sGuidetoGuitars.py
@@ -0,0 +1,8 @@
+# Danny-s-Guide-to-Guitars-
+This is a deep dive into my interests in regard to guitars and the various nuances that revolve around them
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Dannys Guide to Guitars</title>
+<script>
